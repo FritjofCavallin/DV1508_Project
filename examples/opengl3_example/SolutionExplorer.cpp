93dff2b4ba7e6295ccd92a9eae81ef8ed4b3e167
Unknown <the.1man@hotmail.com>
2018-04-25 23:31:06 +0200

    Init branch

--- examples/opengl3_example/SolutionExplorer.cpp
+++ examples/opengl3_example/SolutionExplorer.cpp
@@ -1,23 +1,21 @@
 #include "SolutionExplorer.h"
 
-
-
-SolutionExplorer::SolutionExplorer()
-{
+SolutionExplorer::SolutionExplorer(){
+	winPosX = 0;
+	winPosY = 626;
+	winSizeX = 300;
+	winSizeY = 454;
 }
 
-
-SolutionExplorer::~SolutionExplorer()
-{
+SolutionExplorer::~SolutionExplorer(){
 }
 
-void SolutionExplorer::draw()
-{
+void SolutionExplorer::draw(){
 	// Common stuff
 	ImGui::Begin("SolutionExplorer", 0, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove |
 		ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoSavedSettings | ImGuiWindowFlags_NoTitleBar);
-	ImGui::SetWindowPos(ImVec2(0, 626));
-	ImGui::SetWindowSize(ImVec2(300, 454));
+	ImGui::SetWindowPos(ImVec2(winPosX, winPosY));
+	ImGui::SetWindowSize(ImVec2(winSizeX, winSizeY));
 
 
 
--- examples/opengl3_example/SolutionExplorer.h
+++ examples/opengl3_example/SolutionExplorer.h
@@ -2,11 +2,20 @@
 
 #include "UIContainer.h"
 
+#include <string>
+
 class SolutionExplorer : public UIContainer
 {
 public:
 	SolutionExplorer();
 	virtual ~SolutionExplorer();
+
 	virtual void draw();
+
+protected:
+	//ImGui
+	int winPosX, winPosY, winSizeX, winSizeY;
+
+	//std::vector<Timeline*> timelineVec;
 };
 
