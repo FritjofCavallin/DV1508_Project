6003e9e27e6457d283168db3f37928a8828e056b
Unknown <the.1man@hotmail.com>
2018-04-28 17:10:44 +0200

    Help text added

--- examples/opengl3_example/Properties.cpp
+++ examples/opengl3_example/Properties.cpp
@@ -27,8 +27,8 @@ void Properties::draw(ImVec2 pos, ImVec2 size){
 
 	if(/*currBlock == nullptr*/true){
 		//show help prompt
-		ImGui::Text(CChar(CenterString("KILL ME NOW")));
-		//ImGui::Text("KILL ME NOW");
+		ImGui::Text("\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+		ImGui::Text(CChar(CenterString("Select timeline block to display its properties")));
 	}
 	else{
 		//show block properties
