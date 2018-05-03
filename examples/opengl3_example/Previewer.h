28da73be90e48d05ba6777b232efc6d2d5c212cc
Unknown <deankulenovic@gmail.com>
2018-05-03 17:53:16 +0200

    Fixed previewer just need to change the png
    previewer should be working

--- examples/opengl3_example/Previewer.h
+++ examples/opengl3_example/Previewer.h
@@ -1,12 +1,25 @@
 #pragma once
 
 #include "UIContainer.h"
-
+#include <GL\/gl3w.h>
+#include <GLFW/glfw3.h>
+#define BUFFER_OFFSET(i) ((char *)nullptr + (i))
+#define STB_IMAGE_IMPLEMENTATION
 class Previewer : public UIContainer
 {
 public:
+	GLuint frameBuffer;
+	GLuint gVertexBuffer;
+	GLuint gVertexAttribute;
+	GLuint gShaderProgram;
+	ImVec4 clear_color;
+	unsigned int texture;
+	unsigned int texturen;
 	Previewer(Data* data);
 	virtual ~Previewer();
 	virtual void draw(ImVec2 pos, ImVec2 size);
+	void creationoftexture();
+	void CreateShaders();
+	void CreateTriangleData();
 };
 
