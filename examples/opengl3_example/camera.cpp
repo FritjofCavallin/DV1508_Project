fefe05dbc73613a86616b076df14f909c7f15ccb
Mattias Fredriksson <mattias-fredriksson91@hotmail.se>
2018-05-14 08:40:00 +0200

    Fixed cam rotation speed

--- examples/opengl3_example/camera.cpp
+++ examples/opengl3_example/camera.cpp
@@ -14,8 +14,9 @@ Camera::Camera()
 glm::mat4 Camera::getViewMat()
 {
 	float radius = 10.0f;
-	float camX = sin(glfwGetTime()) * radius;
-	float camZ = cos(glfwGetTime()) * radius;
+	float rotSpeed = 0.15f;
+	float camX = sin(glfwGetTime() * rotSpeed) * radius;
+	float camZ = cos(glfwGetTime() * rotSpeed) * radius;
 	glm::mat4 view;
 	view = glm::lookAt(glm::vec3(camX, 1.f, camZ), glm::vec3(0.0, 0.0, 0.0), glm::vec3(0.0, 1.0, 0.0));
 	return view;
