9fb7c64d09e8ad05473936f759176a95c400acdd
Mattias Fredriksson <mattias-fredriksson91@hotmail.se>
2018-05-15 11:33:58 +0200

    Move texture -> Content/ + Rmvd a texture

--- examples/opengl3_example/Other/Constants.h
+++ examples/opengl3_example/Other/Constants.h
@@ -18,7 +18,7 @@ const static float PARTICLE_DEFAULT_DUR = 5.f;
 const static size_t PARTICLES_EMITTED_MAX = 1000;
 
 
-const static char* DEFAULT_TEX = "error.png";
+const static char* DEFAULT_TEX = "Content/error.png";
 
 
 const float PI = 3.14159265359f;
