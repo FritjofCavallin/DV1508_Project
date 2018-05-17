05b4acc9aaa8b2d31604a82bf32b30c9d39bd45f
Capelliexp <the.1man@hotmail.com>
2018-05-17 16:13:09 +0200

    Spawn block done

--- examples/opengl3_example/Timelines/EmittBlocks/SpawnBlock.cpp
+++ examples/opengl3_example/Timelines/EmittBlocks/SpawnBlock.cpp
@@ -32,4 +32,81 @@ SpawnBlock::~SpawnBlock()
 void SpawnBlock::DrawProperties(ImVec2 pos, ImVec2 size){
 	DrawPropertiesHeader(pos, size);
 
+	//---
+	ImGui::Text("Amount");
+	ImGui::Text("   Initial:");
+	ImGui::SameLine();
+	ImGui::PushItemWidth(45);
+	ImGui::InputFloat("", &_params._initAmount, 0, 0, 3);
+	ImGui::SameLine(120);
+	ImGui::Text("     Final:");
+	ImGui::SameLine();
+	ImGui::PushItemWidth(45);
+	ImGui::InputFloat("   ", &_params._endAmount, 0, 0, 3);
+
+	//---
+	ImGui::Text("Size");
+	ImGui::Text("   Minimum:");
+	ImGui::SameLine();
+	ImGui::PushItemWidth(45);
+	ImGui::InputFloat("x", &_params._minSize.x, 0, 0, 3);
+	ImGui::SameLine();
+	ImGui::InputFloat("y", &_params._minSize.y, 0, 0, 3);
+	ImGui::PopItemWidth();
+
+	ImGui::Text("   Maxumum:");
+	ImGui::SameLine();
+	ImGui::PushItemWidth(45);
+	ImGui::InputFloat("x ", &_params._maxSize.x, 0, 0, 3);
+	ImGui::SameLine(155);
+	ImGui::InputFloat("y ", &_params._maxSize.y, 0, 0, 3);
+	ImGui::PopItemWidth();
+
+	//---
+	ImGui::Text("Origin");
+	ImGui::Text("  ");
+	ImGui::SameLine();
+	ImGui::PushItemWidth(45);
+	ImGui::InputFloat("x  ", &_params._emitOrigin.x, 0, 0, 3);
+	ImGui::SameLine(90);
+	ImGui::InputFloat("y  ", &_params._emitOrigin.y, 0, 0, 3);
+	ImGui::SameLine(153);
+	ImGui::InputFloat("z  ", &_params._emitOrigin.z, 0, 0, 3);
+	ImGui::PopItemWidth();
+
+	//---
+	ImGui::Text("Direction");
+	ImGui::Text("  ");
+	ImGui::SameLine();
+	ImGui::PushItemWidth(45);
+	ImGui::InputFloat("x  ", &_params._emitDir.x, 0, 0, 3);
+	ImGui::SameLine(90);
+	ImGui::InputFloat("y  ", &_params._emitDir.y, 0, 0, 3);
+	ImGui::SameLine(153);
+	ImGui::InputFloat("z  ", &_params._emitDir.z, 0, 0, 3);
+	ImGui::PopItemWidth();
+
+	//---
+	ImGui::Text("Rotation");
+	ImGui::Text("   Min:");
+	ImGui::SameLine();
+	ImGui::PushItemWidth(45);
+	ImGui::InputFloat("    ", &_params._minRotation, 0, 0, 3);
+	ImGui::SameLine(100);
+	ImGui::Text("   Max:");
+	ImGui::SameLine();
+	ImGui::PushItemWidth(45);
+	ImGui::InputFloat("     ", &_params._maxRotation, 0, 0, 3);
+
+	//---
+	ImGui::Text("Force");
+	ImGui::Text("   Min:");
+	ImGui::SameLine();
+	ImGui::PushItemWidth(45);
+	ImGui::InputFloat("    ", &_params._minForce, 0, 0, 3);
+	ImGui::SameLine(100);
+	ImGui::Text("   Max:");
+	ImGui::SameLine();
+	ImGui::PushItemWidth(45);
+	ImGui::InputFloat("     ", &_params._maxForce, 0, 0, 3);
 }
